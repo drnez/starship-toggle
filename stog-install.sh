f89c91f1171ef129a7959a18f73e7e5d8083f596
Shayan Nezami <shayannezami@yahoo.co.uk>
2024-01-28 21:32:32 +0000

    make an alias stog in user's .bashrc to source the main file; update install script to exit in case of errors can to not allow execution as sudo

--- stog-install.sh
+++ stog-install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cp "starship-toggle.sh" "/usr/local/bin"
-chmod +x "/usr/local/bin/starship-toggle.sh"
+set -e
 
-echo "
-#!/bin/bash
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Do not run this script as root!"
+    exit 1
+fi
 
-source /usr/local/bin/starship-toggle.sh
-" > /usr/local/bin/stog
-chmod +x "/usr/local/bin/stog"
+sudo cp "starship-toggle.sh" "/usr/local/bin"
+sudo chmod +x "/usr/local/bin/starship-toggle.sh"
+
+echo "alias stog='source starship-toggle.sh'" >> ~/.bashrc
 
 echo "Installation complete."
